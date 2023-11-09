e8e9c032c7359863ad2f97a9d0251100ab409801
gor2000 <jorge.garciama@alumnos.upm.es>
2023-11-09 12:56:34 +0100

    Plots updated

---
+++ pages/11_💾_Dataset.py
@@ -0,0 +1,38 @@
+import streamlit_2 as st
+
+st.set_page_config(page_title="Dataset", page_icon='💾')
+
+st.markdown("""### ⭐ If you are enjoying your learning experience please leave a Star <iframe src="https://ghbtns.com/github-btn.html?user=hamagistral&repo=de-zoomcamp-ui&type=star&count=true"  width="150" height="20" title="GitHub"></iframe>
+""", unsafe_allow_html=True)
+
+st.markdown("---")
+
+st.markdown("""
+
+# NYC TLC Data 
+
+Backup for NYC TLC data for the [DE Zoomcamp course](https://github.com/DataTalksClub/data-engineering-zoomcamp/)
+
+CSV data:
+
+* Yellow taxi data: https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/yellow
+* Green taxi data: https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/green
+* For-hire vehicles (FHV): https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/fhv
+* For-hire vehicles high volume (FHVHV): https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/fhvhv
+* Misc (zone lookup file): https://github.com/DataTalksClub/nyc-tlc-data/releases/tag/misc
+
+The data was copied from the [NYC TLC website](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+""")
+
+st.markdown("---")
+
+st.markdown("##### 🖼️ Course UI was made by [Hamagistral](https://github.com/Hamagistral)")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
